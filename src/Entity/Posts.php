8804b7d2fee57ee0c260057b06f5aa488e16e321
Sergio-hdD <sergio-hdd@hotmail.com>
2021-01-02 12:55:01 -0300

    8- Creo el PostsController, el PostsType, y cambio para que el campo foto pueda ser null

--- src/Entity/Posts.php
+++ src/Entity/Posts.php
@@ -28,7 +28,7 @@ class Posts
     private $likes;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $foto;
 
