0adb2b87be65d35dfe0c532d0ba65b7bb071ad8f
Sergio-hdD <sergio-hdd@hotmail.com>
2021-01-03 19:01:35 -0300

    11- Consultando Posts en la BD

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Posts;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,16 @@ class DashboardController extends AbstractController
      */
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Posts::class)->findAll();//Traigo todos los posts
+        $postId = $em->getRepository(Posts::class)->find(2);//Traigo el post con id = 2
+        $postTit = $em->getRepository(Posts::class)->findOneBy(['titulo'=>'Primer foto']);//Traigo uno por valor de un campo
+        $postLikes = $em->getRepository(Posts::class)->findBy(['likes'=>'']);//Traigo varios por valor de un campo, en este caso los que tengan los likes vacios
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'Bienvenido al dashboard',
+            'posts' => $posts,
+            'postPorId' => $postId,
+            'postPorTit' => $postTit,
+            'postsPorLikes' => $postLikes
         ]);
     }
 }
