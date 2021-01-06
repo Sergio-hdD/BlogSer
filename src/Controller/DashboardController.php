f68490409e1570c2096256490c41d13d80c8627f
Sergio-hdD <sergio-hdd@hotmail.com>
2021-01-05 21:29:32 -0300

    22- Agrego comentarios, del user logueado, al dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Comentarios;
 use App\Entity\Posts;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,14 +19,17 @@ class DashboardController extends AbstractController
     public function index(PaginatorInterface $paginator, Request $request): Response
     {
         $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
         $queryPosts = $em->getRepository(Posts::class)->traerQueryDeTodosLosPosts();//Traigo la query de todos los posts, consulta personalizada
+        $comentarios = $em->getRepository(Comentarios::class)->findOneBy(['user'=>$user]);//Traigo uno por valor de un campo
         $pagination = $paginator->paginate(
             $queryPosts, /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
             2 /*limit per page (con este último numero le digo de a cuantos muestra en cada página */
         );
         return $this->render('dashboard/index.html.twig', [
-            'pagination' => $pagination
+            'pagination' => $pagination,
+            'comentariosDelUser' => $comentarios
         ]);
     }
 }
